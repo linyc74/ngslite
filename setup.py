99930a440c6e889c794ad6453419ce118e776d45
linyc74 <linyc74@DESKTOP-U42NHK3.localdomain>
2018-11-20 00:56:39 -0500

    Arrange the project file structure for PyPI.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ngslite',
+      version='0.0',
+      description='Light-weight functions for next-generation sequencing (NGS) data analysis',
+      url='https://github.com/linyc74/ngslite',
+      author='Yu-Cheng Lin',
+      author_email='yclin.python@gmail.com',
+      license='MIT',
+      packages=['ngslite'],
+      install_requires=['numpy'],
+      zip_safe=False)
