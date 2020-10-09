cd81d4e06d0f117633436ec0d3a2d2128f29e801
Yu-Cheng Lin <yclin.python@gmail.com>
2020-10-08 21:39:20 -0400

    add some test

---
+++ test/test_random.py
@@ -0,0 +1,32 @@
+import shutil
+from ngslite.random import random_sample
+from .setup import setup_dirs, TestCase
+
+
+class TestRandom(TestCase):
+
+    def setUp(self):
+        self.indir, self.workdir, self.outdir = setup_dirs(__file__)
+
+    def tearDown(self):
+        shutil.rmtree(self.workdir)
+        shutil.rmtree(self.outdir)
+
+    def test_faa(self):
+
+        file = f'{self.indir}/NC_000866.fa'
+        fraction = 0.1
+        output = f'{self.outdir}/NC_000866_{fraction}.fa'
+
+        random_sample(
+            file=file,
+            fraction=fraction,
+            output=output)
+
+        i = 0
+        with open(output) as fh:
+            for line in fh:
+                if line.startswith('>'):
+                    i += 1
+
+        self.assertEqual(27, i)
